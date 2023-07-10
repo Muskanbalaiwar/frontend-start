760b0f160aef303f305913f45ca0be453ede4c18
Muskanbalaiwar <125793580+Muskanbalaiwar@users.noreply.github.com>
2023-07-10 14:58:51 +0530

    Add files via upload

---
+++ components/expenseItem.js
@@ -0,0 +1,16 @@
+function ExpenseItem() {
+  return (
+    <div> 
+      <div>
+        <h2>food</h2>
+        <div>200</div>
+        <h2>petrol</h2>
+        <div>100</div>
+        <h2>movies</h2>
+        <div>2000</div>
+      </div>
+    </div>
+  );
+}
+
+export default ExpenseItem;
