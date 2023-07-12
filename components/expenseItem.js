8920c71491f8a63d833c743b2bb5586b0f524f14
Muskanbalaiwar <125793580+Muskanbalaiwar@users.noreply.github.com>
2023-07-12 13:25:05 +0530

    Add files via upload

--- components/expenseItem.js
+++ components/expenseItem.js
@@ -1,15 +1,13 @@
-import './expenseItem.css'
+import "./expenseItem.css";
+import ExpenseDate from "./expenseDate";
+import ExpenseDetails from "./expenseDetails";
 
 function ExpenseItem(props) {
+  
   return (
     <div className="expense-item">
-      <div>{props.date.toISOString()}</div>
-      <div className="expense-item__description">
-        <h2>{props.title}</h2>
-        <div>{props.location}</div>
-        <div className="expense-item__price">Rs {props.amount}</div>
-       
-      </div>
+      <ExpenseDate date = {props.date}/>
+      <ExpenseDetails  amount={props.amount}  location={props.location}   title={props.title} />
     </div>
   );
 }
