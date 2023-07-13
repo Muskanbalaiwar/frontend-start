918fa82a2804f3920ed65bc72000ea07af294294
Muskanbalaiwar <125793580+Muskanbalaiwar@users.noreply.github.com>
2023-07-13 08:23:42 +0530

    Add files via upload

--- components/Expenses/expenseItem.js
+++ components/Expenses/expenseItem.js
@@ -1,14 +1,28 @@
 import "./expenseItem.css";
 import ExpenseDate from "./expenseDate";
 import ExpenseDetails from "./expenseDetails";
-import Card from "../UI/card";
+//import Card from "../UI/card";
+import React ,{useState} from 'react';
 
 const ExpenseItem=(props) =>{
+const [title ,setTitle]  = useState(props.title)
+const [amount , setAmount] =useState(props.amount);
+  const clickHandler = ()=>{
+    setTitle('updated')
+  }
+
+  const amountHandler = ()=>{
+    setAmount(100)
+  }
+
   
   return (
     <div className="expense-item">
       <ExpenseDate date = {props.date}/>
-      <ExpenseDetails  amount={props.amount}  location={props.location}   title={props.title} />
+      <ExpenseDetails  amount={amount}
+       location={props.location}   title={title} />
+       <button onClick={amountHandler}>change amount</button>
+      <button onClick={clickHandler}>Change Title</button>
     </div>
   );
 }
