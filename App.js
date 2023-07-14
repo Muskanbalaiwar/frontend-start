1b8d6132dc35cb111c7c851a778b1d215a6a0cde
Muskanbalaiwar <125793580+Muskanbalaiwar@users.noreply.github.com>
2023-07-14 18:31:26 +0530

    Add files via upload

--- App.js
+++ App.js
@@ -1,43 +1,51 @@
 import ExpenseItem from "./components/Expenses/expenseItem";
 import "./App.css";
 import './components/Expenses/expenses.css'
+import Card from './components/UI/card'
+import NewExpense from './components/newExpense/newExpense'
+
+
 
  const App=() =>{
   const expense = [
-    { title: "car Insurance", amount: 200, date: new Date(2023, 3, 12), location:"agra" },
+    { title: "car Insurance", amount: 200, date: new Date(2023, 3, 12), location:"agra" ,id:'id1'},
   ];
   
 
   
   return (
-    <div className="expenses">
-      <ExpenseItem
+    <div>
+    <NewExpense></NewExpense>
+    <Card className="expenses">
+      <ExpenseItem id={expense[0].id}
         title={expense[0].title}
         amount={expense[0].amount}
         date={expense[0].date}
         location = {expense[0].location}
       ></ExpenseItem>
-      <ExpenseItem
+      <ExpenseItem id='id2'
         title={expense[0].title}
         amount={expense[0].amount}
         date={expense[0].date}
         location = {expense[0].location}
       ></ExpenseItem>
-      <ExpenseItem
+      <ExpenseItem id='id3'
         title={expense[0].title}
         amount={expense[0].amount}
         date={expense[0].date}
         location = {expense[0].location}
       ></ExpenseItem>
-      <ExpenseItem
+      <ExpenseItem id='id4'
         title={expense[0].title}
         amount={expense[0].amount}
         date={expense[0].date}
         location = {expense[0].location}
       ></ExpenseItem>
       
+    </Card>
     </div>
-  );}
+  );
+}
 
 
 export default App;
--- components/UI/card.js
+++ components/UI/card.js
@@ -1,10 +1,10 @@
 import './card.css'
+import React from 'react';
 
-function Card(props){
-    const classes ='Card' + props.className
-return(
-<div className={classes}>{props.children}</div>
-)
-}
+const Card = (props) => {
+    const classes = 'card ' + props.className;
+  
+    return <div className={classes}>{props.children}</div>;
+  };
 
 export default Card
