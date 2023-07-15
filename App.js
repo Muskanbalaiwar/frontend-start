d6cc65a704ad0456265e0fb84331bc50f712bf37
Muskanbalaiwar <125793580+Muskanbalaiwar@users.noreply.github.com>
2023-07-15 15:32:12 +0530

    Add files via upload

--- App.js
+++ App.js
@@ -1,51 +1,56 @@
 import ExpenseItem from "./components/Expenses/expenseItem";
 import "./App.css";
-import './components/Expenses/expenses.css'
-import Card from './components/UI/card'
-import NewExpense from './components/newExpense/newExpense'
+import "./components/Expenses/expenses.css";
+import Card from "./components/UI/card";
+import NewExpense from "./components/newExpense/newExpense";
 
+import ExpensesFilter from "./components/Expenses/expenseFilter";
 
+import React, { useState } from "react";
 
- const App=() =>{
-  const expense = [
-    { title: "car Insurance", amount: 200, date: new Date(2023, 3, 12), location:"agra" ,id:'id1'},
-  ];
-  
+const dummy_expenses = [
+  {
+    title: "car Insurance",
+    amount: 200,
+    date: new Date(2023, 3, 12),
+    id: "id1",
+  },
+];
 
-  
+const App = () => {
+ const [expenses,setExpenses] =useState(dummy_expenses)
+
+  const addExpenseHadler = (expense) => {
+    setExpenses((prevExpenses)=>{
+      return [expense,...prevExpenses]
+    })
+  };
+
+  const [filteredYear, setFilteredYear] = useState("2020");
+
+  const filterChangeHandler = (selectedYear) => {
+    setFilteredYear(selectedYear);
+  };
+
   return (
     <div>
-    <NewExpense></NewExpense>
-    <Card className="expenses">
-      <ExpenseItem id={expense[0].id}
-        title={expense[0].title}
-        amount={expense[0].amount}
-        date={expense[0].date}
-        location = {expense[0].location}
-      ></ExpenseItem>
-      <ExpenseItem id='id2'
-        title={expense[0].title}
-        amount={expense[0].amount}
-        date={expense[0].date}
-        location = {expense[0].location}
-      ></ExpenseItem>
-      <ExpenseItem id='id3'
-        title={expense[0].title}
-        amount={expense[0].amount}
-        date={expense[0].date}
-        location = {expense[0].location}
-      ></ExpenseItem>
-      <ExpenseItem id='id4'
-        title={expense[0].title}
-        amount={expense[0].amount}
-        date={expense[0].date}
-        location = {expense[0].location}
-      ></ExpenseItem>
-      
-    </Card>
+      <NewExpense onAddExpense={addExpenseHadler} />
+      <Card className="expenses">
+        <ExpensesFilter
+          selected={filteredYear}
+          onChangeFilter={filterChangeHandler}
+        />
+        {expenses.map((expense) => {
+          return(<ExpenseItem
+          key ={expense.id}
+            title={expense.title}
+            amount={expense.amount}
+            date={expense.date}
+          />);
+        })}
+      </Card>
     </div>
   );
-}
-
+};
 
 export default App;
